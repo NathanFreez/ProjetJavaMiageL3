453eb0567eb9935d30b64581011f671e03e9534c
Unknown <nat_2807@hotmail.fr>
2018-02-12 15:29:07 +0100

    Ajout interface/classe mere
    Ajout d'une interface IFichier et d'une classe mere ChargerEntrepriseCSV (pour des fichiers de type csv)

--- src/projetmiagel3/IFichier.java
+++ src/projetmiagel3/IFichier.java
@@ -5,9 +5,22 @@
  */
 package projetmiagel3;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author VIALA Guillaume
  */
 public interface IFichier {
+    public abstract void chargerEmploye()throws FileNotFoundException;
+    public abstract void chargerCompetence()throws FileNotFoundException;
+    public abstract void chargerCompetenceEmploye()throws FileNotFoundException;
+    public abstract void chargerMission() throws FileNotFoundException;
+    public abstract void chargerCompetenceMission() throws FileNotFoundException;
+    public abstract void sauvegarderEmploye(ArrayList<Employe> listE) throws IOException;
+    public abstract void sauvegarderCompetence(ArrayList<Competence> listC) throws IOException;
+    public abstract void sauvegarderMission(ArrayList<Mission> listM) throws IOException;
+    public abstract void sauvegardeCompetenceMission(ArrayList<Mission> listM) throws IOException;
 }
